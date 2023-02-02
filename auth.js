08d5c6a928b5b8348896b483efd60301d368de31
Milos <milos.mijuskovic@fsdc.info>
2023-02-02 11:54:01 +0100

    Added authentication

---
+++ auth.js
@@ -0,0 +1,9 @@
+function authenticate(login , password) {
+if (login === 'login' && password === 'password') {
+
+        return 'You are logged in';
+
+        } else {
+            return 'Login is not correct'
+        }
+    }
