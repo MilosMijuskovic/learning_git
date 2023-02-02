ffb95151a96f527c077d5500fea1ecab3b93fe0d
Milos <milos.mijuskovic@fsdc.info>
2023-02-02 12:09:08 +0100

    Fixed text for welcome

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 function authenticate(login , password) {
 if (login === 'login' && password === 'password') {
 
-        return 'You are logged in!';
+        return 'Welcome!';
 
         } else {
             return 'Login is not correct';
